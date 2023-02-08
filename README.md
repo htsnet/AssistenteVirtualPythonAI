91c8716cf206831194368724a196f2161216fa33
Your NameHamilton <hamilton@hts-net.com.br>
2023-02-08 16:58:36 -0300

    Final project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Projeto básico de Assistente Virtual em Python e AI
 base: curso da Danki Code
+
+O projeto usa a IA do Google para reconhecimento de voz em língua portuguesa (Brasil).
+A voz usada é a da "Luciana" da Apple.
--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import re
+import pyttsx3
+
 
 # variável para guardar o nome
 nome = ''
@@ -7,9 +9,12 @@ while True:
     mic = sr.Recognizer()
 
     with sr.Microphone() as source:
+        engine = pyttsx3.init()
+        engine.setProperty('voice', 'com.apple.speech.synthesis.voice.luciana')
         mic.adjust_for_ambient_noise(source)
         
         if nome != '':
+            # se já tem nome, já trata de forma pessoal
             print(nome+", você pode falar alguma coisa agora.")
         else:
             print("Vamos começar. Fale alguma coisa.")
@@ -20,11 +25,15 @@ while True:
             frase = mic.recognize_google(audio, language='pt-BR')
             
             if re.search(r'\b'+'ajudar'+r'\b',format(frase)):
+                engine.say('Ajuda')
+                engine.runAndWait()
                 print('Algo relacionado a ajudar')
             elif re.search(r'\b'+'meu nome é'+r'\b',format(frase)):
                 t = re.search('meu nome é (.*)', format(frase))
                 nome = t.group(1)
                 print("seu nome é "+nome)
+                engine.say("Nome falado foi "+ nome)
+                engine.runAndWait()
                     
                 
             print('você falou:' + frase)
